182e34344063d2765f7bc0d5fc3aa26d10cb5771
Ayash-Bera <ayashbera@gmail.com>
2025-10-19 22:05:49 +0530

    Revert "updates for the intent functions"
    This reverts commit 8332216d4ccbed2ec66484f6e2fb7db3006f1a2c.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,10 @@
 import type { PublicClient, WalletClient } from "wagmi";
-import type {
-  DecryptedTransaction,
-  EERCHookResult,
-  IntentExecutionData,
-  SubmitIntentResult,
-  IntentStatus,
-} from "./hooks";
+import type { DecryptedTransaction, EERCHookResult } from "./hooks";
 
 export { EERC } from "./EERC";
 export { useEERC } from "./hooks";
 export { Poseidon } from "./crypto";
-export {
-  saveIntentExecutionData,
-  getIntentExecutionData,
-  getAllStoredIntents,
-  removeIntentExecutionData,
-  clearAllIntents,
-  exportIntents,
-  importIntents,
-} from "./utils/intentStorage";
 
 export type { PublicClient as CompatiblePublicClient };
 export type { WalletClient as CompatibleWalletClient };
-export type {
-  DecryptedTransaction,
-  EERCHookResult,
-  IntentExecutionData,
-  SubmitIntentResult,
-  IntentStatus,
-};
+export type { DecryptedTransaction, EERCHookResult };
